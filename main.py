a2a5b0ff6a833a559dcfbea2226bb3e96e2d9e35
Dmitry <markelovdima408@gmail.com>
2024-03-24 15:37:14 +0400

    open even picture

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import base64
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys 
+from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Chrome()
 
@@ -20,23 +21,28 @@ def scroll_page():
     driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
 
 def fetch_images(max_images = 10):
-    filtered_images = []
+    elements = []
     image_urls = []
     driver.get(url)
 
-    while len(filtered_images) < max_images:
+    while len(elements) < max_images:
         scroll_page()
         time.sleep(1)
 
-        all_image = driver.find_elements(By.CLASS_NAME, 'rg_i')
-        for img in all_image:
-            if img.get_attribute('src') is not None:
-                filtered_images.append(img)
-    
-    for img in all_image:
-        image_urls.append(img.get_attribute('src'))
+        elements = driver.find_elements(By.CLASS_NAME, 'rg_i')
+
+    for element in elements:
         if len(image_urls) >= max_images:
             break
+        try:
+            element.click()
+            time.sleep(0.5)
+            img = driver.find_element(By.CLASS_NAME, 'iPVvYb' if 'iPVvYb' else 'sFlh5c')
+            image_urls.append(img.get_attribute('src'))
+            continue
+        except:
+            image_urls.append(element.get_attribute('src'))
+            continue
     
     driver.quit()
     return image_urls
@@ -59,5 +65,5 @@ def download_images(images_urls):
                 file.write(plain_data)
             save_images+=1
 
-img_urls = fetch_images(50)
+img_urls = fetch_images(505)
 download_images(img_urls)
