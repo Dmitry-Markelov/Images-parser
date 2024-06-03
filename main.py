e488b802aaa9b7e1a40967fd7b6d440885138875
Dmitry <117797846+Dmitry-Markelov@users.noreply.github.com>
2024-06-03 23:24:52 +0400

    final commit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import os
 import requests
 import time
 import base64
+import pymysql
+from PIL import Image
+from io import BytesIO
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,6 +11,19 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+
+# Параметры подключения к базе данных
+db_config = {
+    'host': 'localhost',
+    'user': 'username',
+    'password': 'password',
+    'database': 'mydb',
+    'charset': 'utf8mb4',
+    'cursorclass': pymysql.cursors.DictCursor
+}
+
+# Подключение к базе данных
+connection = pymysql.connect(**db_config)
 
 # заголовки запроса
 HEADERS = {
@@ -21,17 +36,11 @@ driver = webdriver.Firefox()
 QUERY = 'car side view'
 URL = f'https://www.google.com/search?q={QUERY}&udm=2&tbm=isch'
 
-# путь для сохраниения изображений
-SAVE_PATH = 'images/image'
-
 # максимальное количество изображений для загрузки
 MAX_IMAGES = 1000
 
 # ожидание загрузки
 wait = WebDriverWait(driver, 5)
-
-if not os.path.exists('images'):
-    os.makedirs('images') # создание папки с результатами
 
 def scroll_page():
     try:
@@ -45,18 +54,16 @@ def fetch_images(max_images = 10):
     driver.get(URL)
     index = 0
 
-    def add_src(index): # поиск картинки и проверка на существующий src
+    def add_src(index):
         try:
             img = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'iPVvYb')))
-        except:
-            try:
-                img = driver.find_element(By.CLASS_NAME, 'sFlh5c')
-            except:
-                return print('Не удалось найти изображение')
-        src = img.get_attribute('src')
-        if src not in image_urls:
-            image_urls.append(src) # добавление ссылки в массив
-            download_image(src, index)
+            src = img.get_attribute('src')
+            if src not in image_urls:
+                image_urls.append(src)
+                tags = extract_tags(img)
+                store_image_in_db(src, index, tags)
+        except Exception as e:
+            print(f'Не удалось найти изображение: {e}')
 
     while len(elements) < max_images/10:
         scroll_page()
@@ -91,24 +98,61 @@ def fetch_images(max_images = 10):
     driver.quit()
     return image_urls
 
-def download_image(src, index): # скачивание изображения по URL
+def extract_tags(img_element):
+    tags = []
+    alt_text = img_element.get_attribute('alt')
+    title_text = img_element.get_attribute('title')
+    if alt_text:
+        tags.append(alt_text)
+    if title_text:
+        tags.append(title_text)
+    return tags
+
+def store_image_in_db(src, index, tags):
     try:
-        save_path = f'{SAVE_PATH}{index}.jpg'
+        if src.startswith('https'):
+            img = requests.get(src, headers=HEADERS).content
+        elif src.startswith('data:image'):
+            data = src.split(',', 1)[-1]
+            img = base64.b64decode(data)
+        else:
+            print('Не удалось получить изображение')
+            return
 
-        with open(save_path, "wb") as file:
-            if src.startswith('https'): # проверка, начинается ли URL с 'https'
-                img = requests.get(src, headers=HEADERS)
-                file.write(img.content)
-            elif src.startswith('data:image'): # проверка, начинается ли URL с 'https'
-                data = src.split(',', 1)[-1]
-                plain_data = base64.b64decode(data)
-                file.write(plain_data)
-    except:
-        print('Не удалось скачать изображение')
+        # Извлечение метаданных изображения
+        image = Image.open(BytesIO(img))
+        width, height = image.size
+        size = len(img)
+        format = image.format
 
-def download_images(images_urls): # скачивание всех изображений из массива
-    for index, src in enumerate(images_urls):
-        download_image(src, index)
+        # Сохранение метаданных и URL в базу данных
+        with connection.cursor() as cursor:
+            sql = """
+            INSERT INTO image (url, name, width, height, size, format)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """
+            cursor.execute(sql, (src, f'image{index}', width, height, size, format))
+            image_id = cursor.lastrowid
+
+            # Сохранение тегов в базу данных
+            for tag in tags:
+                sql = """
+                INSERT INTO tags (tag_name)
+                VALUES (%s)
+                ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id)
+                """
+                cursor.execute(sql, (tag,))
+                tag_id = cursor.lastrowid
+
+                sql = """
+                INSERT INTO image_tag (id_image, id_tag)
+                VALUES (%s, %s)
+                """
+                cursor.execute(sql, (image_id, tag_id))
+
+        connection.commit()
+    except Exception as e:
+        print(f'Не удалось сохранить изображение в базу данных: {e}')
 
 # получение массива ссылок
 img_urls = fetch_images(MAX_IMAGES)
